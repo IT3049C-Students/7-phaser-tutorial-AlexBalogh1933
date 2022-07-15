3f5184ace05c94c56d6aa17ee57459680bc0462e
Alex Balogh <baloghaj@mail.uc.edu>
2022-07-15 10:51:31 -0400

    Part 3

--- Scene1.js
+++ Scene1.js
@@ -2,7 +2,14 @@ class Scene1 extends Phaser.Scene {
     constructor() {
         super("bootGame");
     }
-    
+
+    preload() {
+        this.load.image("background", "assets/images/background.png");
+        this.load.image("ship", "assets/image/ship.png")
+        this.load.image("ship2", "assets/image/ship2.png")
+        this.load.image("ship3", "assets/image/ship3.png")
+    }
+
     create() {
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
