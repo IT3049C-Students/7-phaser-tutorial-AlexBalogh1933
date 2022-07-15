83a451dcf7928d67ce4889efe7bc85d24f1ed80e
Alex Balogh <baloghaj@mail.uc.edu>
2022-07-15 11:44:36 -0400

    Part 6

--- game.js
+++ game.js
@@ -3,7 +3,14 @@ window.onload = function(){
         width: 256,
         height: 272,
         backgroundColor: 0x000000,
-        scene: [Scene1, Scene2]
+        scene: [Scene1, Scene2],
+        pixelArt: true,
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: false
+            }
+        }
     }
     var game =new Phaser.Game(config);
 }
